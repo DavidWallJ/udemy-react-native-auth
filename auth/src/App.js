b4f46e1693cb1e8d1c1b2a9b07d1287a0b262d90
DavidWallJ <djkindread@gmail.com>
2017-07-19 18:15:53 +0800

    Initial commit

---
+++ auth/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDXcemd-pX-3g2J4SxakQddsT5lNnoZ_6A",
+      authDomain: "udemy-react-native-auth-76767.firebaseapp.com",
+      databaseURL: "https://udemy-react-native-auth-76767.firebaseio.com",
+      projectId: "udemy-react-native-auth-76767",
+      storageBucket: "udemy-react-native-auth-76767.appspot.com",
+      messagingSenderId: "584400790082"
+    })
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication"/>
+        <LoginForm/>
+      </View>
+    );
+  }
+}
+
+export default App;
